5e50a992cd368f208b5f7d8a98ed87364a800ff4
Pedro <pmapedro1510@gmail.com>
2025-04-02 21:38:23 -0300

    primeiros endpoints

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const dns = require('dns');
 
 const senhaCodificada = encodeURIComponent('U-abjbNXGrPT7?v');
 
@@ -7,8 +8,9 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false,
   },
-  lookup: (hostname, options, callback) => {
-    return dns.lookup(hostname, { family: 4 }, callback);
+  // Força uso de IPv4 (impede ENETUNREACH ao usar Supabase via Railway)
+  dnsLookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4 }, callback);
   },
 });
 
