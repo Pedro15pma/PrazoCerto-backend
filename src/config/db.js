60167498e69cc2821b2751e38a952afff18ce025
Pedro <pmapedro1510@gmail.com>
2025-04-02 21:44:10 -0300

    primeiros endpoints

--- src/config/db.js
+++ src/config/db.js
@@ -5,13 +5,10 @@ const senhaCodificada = encodeURIComponent('U-abjbNXGrPT7?v');
 
 const pool = new Pool({
   connectionString: `postgresql://postgres:${senhaCodificada}@db.pszuaetwtgxturpykkwo.supabase.co:5432/postgres`,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  // Força uso de IPv4 (impede ENETUNREACH ao usar Supabase via Railway)
-  dnsLookup: (hostname, options, callback) => {
+  ssl: { rejectUnauthorized: false },
+  lookup: (hostname, options, callback) => {
     dns.lookup(hostname, { family: 4 }, callback);
-  },
+  }
 });
 
 module.exports = pool;
