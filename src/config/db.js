888e335f193e83f8fd03201f8b4f6415d99ad36e
Pedro <pmapedro1510@gmail.com>
2025-06-24 01:04:52 -0300

    tratamento de outras issues

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,15 @@ const { Pool } = require('pg');
 
 require('dotenv').config();
 
-const DATABASE = encodeURIComponent(process.env.DATABASE)
+const DATABASE = encodeURIComponent(process.env.DATABASE);
 
 /*const pool = new Pool({
   connectionString: 'postgresql://postgres:1234@localhost:5432/PrazoCerto',
 });*/
 
-
 const pool = new Pool({
   connectionString: `postgresql://postgres.pszuaetwtgxturpykkwo:${DATABASE}@aws-0-sa-east-1.pooler.supabase.com:5432/postgres`,
+  // connectionString: `${DATABASE}`,
   ssl: { rejectUnauthorized: false },
 });
 module.exports = pool;
-
